737426e6f64ff0b473f33188f5f9aecd1d95ed8f
joojdev <jvpiovezan2905@gmail.com>
2025-02-24 14:35:20 -0300

    New methods

--- frontend/main.js
+++ frontend/main.js
@@ -4,3 +4,19 @@ const toolbox = {
 }
 
 const workspace = Blockly.inject('blocklyDiv', { toolbox });
+
+const projectsListElement = document.querySelector('#projects');
+
+function updateProjectsList() {
+  eel.list_projects()().then((projects) => {
+    projectsListElement.innerHTML = '';
+
+    projects.forEach((project) => {
+      const projectElement = document.createElement('li');
+      projectElement.textContent = project;
+      projectsListElement.append(projectElement);
+    });
+  })
+}
+
+setInterval(updateProjectsList, 1000);
--- main.py
+++ main.py
@@ -1,4 +1,17 @@
-import eel
+import eel, os
+
+projects_folder = 'projects'
+
+if not os.path.exists(projects_folder):
+  os.mkdir(projects_folder)
+
+@eel.expose
+def list_projects():
+  return os.listdir(projects_folder)
+
+@eel.expose
+def new_project(project_name):
+  os.mkdir(os.path.join(projects_folder, project_name))
 
 eel.init('frontend')
 eel.start('index.html')
