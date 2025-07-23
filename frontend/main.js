21bf7afce125b9048db936c4b44fafa140635dd6
joojdev <jvpiovezan2905@gmail.com>
2025-07-22 23:51:32 -0300

    Added project name prompt before adding a project

--- frontend/main.js
+++ frontend/main.js
@@ -7,8 +7,12 @@ const workspace = Blockly.inject('blocklyDiv', { toolbox });
 
 const projectsListElement = document.querySelector('#projects');
 
+let projectList = [];
+
 function updateProjectsList() {
   eel.list_projects()().then((projects) => {
+    projectList = projects;
+
     projectsListElement.innerHTML = '';
 
     projects.forEach((project) => {
@@ -16,7 +20,46 @@ function updateProjectsList() {
       projectElement.textContent = project;
       projectsListElement.append(projectElement);
     });
-  })
+  });
 }
 
-setInterval(updateProjectsList, 1000);
+const newProjectButton = document.querySelector('#newProjectButton');
+const blackScreenElement = document.querySelector('#blackScreen');
+const projectNameForm = document.querySelector('#projectNameForm');
+const cancelProjectNameFormButton = document.querySelector('#cancelProjectNameFormButton');
+const projectNameInput = document.querySelector('#projectNameInput');
+
+function showProjectNamePrompt() {
+  blackScreenElement.classList.remove('hidden');
+  projectNameForm.classList.remove('hidden');
+}
+
+function hideProjectNamePrompt() {
+  blackScreenElement.classList.add('hidden');
+  projectNameForm.classList.add('hidden');
+}
+
+newProjectButton.addEventListener('click', () => {
+  showProjectNamePrompt();
+});
+
+cancelProjectNameFormButton.addEventListener('click', () => {
+  hideProjectNamePrompt();
+});
+
+projectNameForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  
+  const projectName = projectNameInput.value.trim();
+
+  if (!projectName) return;
+  if (projectList.includes(projectName)) return;
+
+  projectNameInput.value = '';
+
+  eel.new_project(projectName);
+
+  hideProjectNamePrompt();
+});
+
+setInterval(updateProjectsList, 500);
